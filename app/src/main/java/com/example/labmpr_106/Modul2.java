93a38a0701a140bfe0a9582983c68ed14aaca41b
zakiburnama <zaki.burnama1017@gmail.com>
2021-11-02 17:44:39 +0700

    new action bar

--- app/src/main/java/com/example/labmpr_106/Modul2.java
+++ app/src/main/java/com/example/labmpr_106/Modul2.java
@@ -1,8 +1,14 @@
 package com.example.labmpr_106;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Modul2 extends AppCompatActivity {
 
@@ -10,5 +16,48 @@ public class Modul2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modul2);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar2);
+        setSupportActionBar(myToolbar);
+    }
+
+    public boolean onCreateOptionMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(Modul2.this, query, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemsSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_help:
+                Toast.makeText(this, "Help", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_setting:
+                Toast.makeText(this, "Setting", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_about:
+                Toast.makeText(this, "About", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
