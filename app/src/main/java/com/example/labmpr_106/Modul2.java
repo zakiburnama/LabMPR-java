2bea9fccc886592814640bc7e8aea75d8d8b684f
zakiburnama <zaki.burnama1017@gmail.com>
2021-11-03 08:37:39 +0700

    brand new action bar

--- app/src/main/java/com/example/labmpr_106/Modul2.java
+++ app/src/main/java/com/example/labmpr_106/Modul2.java
@@ -1,14 +1,19 @@
 package com.example.labmpr_106;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.MenuItemCompat;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class Modul2 extends AppCompatActivity {
 
@@ -17,11 +22,15 @@ public class Modul2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modul2);
 
+        //MODUL 3
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar2);
         setSupportActionBar(myToolbar);
+        getSupportActionBar().setTitle("");
+
     }
 
-    public boolean onCreateOptionMenu(Menu menu){
+    //MODUL 3
+    public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu_main,menu);
 
         MenuItem searchItem = menu.findItem(R.id.action_search);
@@ -42,7 +51,7 @@ public class Modul2 extends AppCompatActivity {
         return super.onPrepareOptionsMenu(menu);
     }
 
-    public boolean onOptionsItemsSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_help:
                 Toast.makeText(this, "Help", Toast.LENGTH_SHORT).show();
@@ -60,4 +69,51 @@ public class Modul2 extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+    //END MODUL 3
+
+    //MODUL 4
+    public void snackbar (View view) {
+        Snackbar snackbar = Snackbar.make(
+                findViewById(R.id.constraintLayout),
+                "Ini adalah snackbar, iyeay",
+                Snackbar.LENGTH_SHORT
+        );
+        snackbar.show();
+    }
+
+    public void showlist (View view) {
+        final
+        String[] warna = {"Merah", "Kuning", "Hijau", "Biru"};
+        AlertDialog.Builder showlist = new AlertDialog.Builder(this);
+        showlist.setItems(warna, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Snackbar snackbar = Snackbar.make(
+                        findViewById(R.id.constraintLayout),
+                        "Anda memilih warna "+warna[i],
+                        Snackbar.LENGTH_SHORT
+                );
+                snackbar.show();
+            }
+        }).setTitle("Pilih warna yang anda sukai");
+        showlist.create().show();
+    }
+
+    public void close (View view) {
+        final AlertDialog.Builder tutup = new AlertDialog.Builder(this);
+        tutup.setMessage("Yakin keluar?")
+                .setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                }).create().show();
+    }
+    //END MODUL 4
 }
